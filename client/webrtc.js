3730d4fc6e49aacaaa045d866047ff910fcb1cff
Dila <dila@vurung.com>
2018-11-17 21:27:00 +0545

    test

--- client/webrtc.js
+++ client/webrtc.js
@@ -7,7 +7,7 @@ var serverConnection;
 
 var peerConnectionConfig = {
   'iceServers': [
-    //{'urls': 'stun:stun.stunprotocol.org:3478'},
+    {'urls': 'stun:stun.l.google.com:3478'},
     {'urls': 'stun:stun.l.google.com:19302'},
   ]
 };
